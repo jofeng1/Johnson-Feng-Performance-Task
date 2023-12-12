b5a4bc0f452c0388bf76fffd8696e7a48daa488a
jofeng1 <jofeng@s.sfusd.edu>
2023-12-12 12:38:51 -0800

    sound effects added

--- Player.gd
+++ Player.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
 @export var speed = 400
+@onready var b_gmusic = $"../BGmusic"
+@onready var enemy_hit = $"../EnemyHit"
 
-
+func _ready():
+	b_gmusic.play()
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
@@ -23,3 +26,5 @@ func _process(delta):
 
 func _on_body_entered(body):
 	hide()
+	enemy_hit.play()
+	b_gmusic.stop()
